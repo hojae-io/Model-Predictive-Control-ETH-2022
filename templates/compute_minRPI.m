ef25de56629ad8893d8e516c67f430f0d96be312
hojae-io <hleeho@student.ethz.ch>
2022-05-13 17:48:38 +0200

    28

--- templates/compute_minRPI.m
+++ templates/compute_minRPI.m
@@ -7,16 +7,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [H_tube,h_tube,n_iter] = compute_minRPI(K_tube,params)
-    % YOUR CODE HERE
-    params
-    params.model
-    params.constraints
-    
+    % YOUR CODE HERE    
     A = params.model.A;
     B = params.model.B;
-    w = params.constraints.MaxAbsDisturbance;
-    A, B, w
+    Hw = params.constraints.DisturbanceMatrix;
+    hw = params.constraints.DisturbanceRHS;
+    W = Polyhedron(Hw, hw);
     
-    e0 = 0;
+    Acl = A+B*K_tube;
+    E_old = W;
+  
+    for i=1:1000
+        E_new = plus(E_old, Acl^i*W);
+        E_new.minHRep();
+        
+        if eq(E_new, E_old)
+            n_iter = i;
+            H_tube = E_new.A;
+            h_tube = E_new.b;
+            break
+        end
+        
+        E_old = E_new;
+    end
     
 end
